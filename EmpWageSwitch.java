838ad6de8a25c042b7ab92842a05c4206cd973e7
anish <anishsalgia1997@gmail.com>
2020-12-23 14:04:43 +0530

    Add: Added code using switch case

---
+++ EmpWageSwitch.java
@@ -0,0 +1,37 @@
+public class EmpWageSwitch
+{
+	public static final int FULL_TIME=1;
+	public static final int PART_TIME=2;
+	public static final int EMP_RATE_PER_HR=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
+
+	public static void main(String[] args)
+	{		
+		int empHrs=0;
+		int totalEmpWage=0;
+		int empWage=0;
+
+		for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+		{
+			int empCheck=(int)Math.floor(Math.random()*10)%3;
+
+			switch(empCheck)
+			{
+				case FULL_TIME:
+				empHrs=8;
+				break;
+			
+				case PART_TIME:
+				empHrs=4;
+				break;
+		
+				default:
+				empHrs=0;
+			}
+			empWage=empHrs*EMP_RATE_PER_HR;
+			totalEmpWage+=empWage;
+		}
+
+		System.out.println("Total Employee Wage: "+totalEmpWage);
+	}
+}
